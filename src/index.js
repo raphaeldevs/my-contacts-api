07ae109efa5e75cb2660f37709b26ab349c9c97b
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-12-12 16:06:04 -0300

    feat: hello express! :)

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (request, response) => {
+  response.send('Hello Express')
+})
+
+app.listen(3000, () =>
+  console.log('🎈 Server started at http://localhost:3000')
+)
