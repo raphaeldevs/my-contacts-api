5935f433407f78044f939d55f3b0ef6dba267932
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-12-12 16:37:11 -0300

    chore: add eslint and editor config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
 app.get('/', (request, response) => {
-  response.send('Hello Express')
-})
+  response.send('Hello Express');
+});
 
-app.listen(3000, () =>
-  console.log('🎈 Server started at http://localhost:3000')
-)
+app.listen(3000, () => console.log('🎈 Server started at http://localhost:3000'));
