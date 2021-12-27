b3b7e2a030974ae05f21e77b09d6155216e83061
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-12-27 00:22:07 -0300

    refactor: router name

--- src/routes.js
+++ src/routes.js
@@ -11,10 +11,10 @@ router.post("/contacts", ContactController.store);
 router.put("/contacts/:id", ContactController.update);
 router.delete("/contacts/:id", ContactController.delete);
 
-router.get("/category", CategoryController.index);
-router.get("/category/:id", CategoryController.show);
-router.post("/category", CategoryController.store);
-router.put("/category/:id", CategoryController.update);
-router.delete("/category/:id", CategoryController.delete);
+router.get("/categories", CategoryController.index);
+router.get("/categories/:id", CategoryController.show);
+router.post("/categories", CategoryController.store);
+router.put("/categories/:id", CategoryController.update);
+router.delete("/categories/:id", CategoryController.delete);
 
 module.exports = router;
